aed2bf0089cb693e3ffd41e5ec131bd9be047468
JoseOlguin <movistar.8331560462@gmail.com>
2024-07-24 13:56:44 -0600

    Commit for: stage0-module4-conditions-task3

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        if (character >= 'a') {
+            character -= ('a' - 'A');
+        }
+
+        if (character >= 'A' && character <= 'Z') {
+            if (character == 'A' || character == 'E' || character == 'I' || character == 'O' || character == 'U') {
+                System.out.println("Vowel");
+            } else {
+                System.out.println("Consonant");
+            }
+        } else {
+            System.out.println("wrong alphabet!");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        if (salary < 0.0f) {
+            System.out.println("wrong input!");
+        } else if (salary > 20000.0f) {
+            System.out.println(salary * (1.0f - 0.20f));
+        } else if (salary > 10000) {
+            System.out.println(salary * (1.0f - 0.18f));
+        } else {
+            System.out.println(salary * (1.0f - 0.15f));
+        }
     }
 }
